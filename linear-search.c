e3b19ef8e5f97202b39e4c0ab99c43d331fa07bd
Xtronix90 <sujaljainedu2002@gmail.com>
2023-04-01 19:28:52 +0530

    implemented both function

--- linear-search.c
+++ linear-search.c
@@ -7,14 +7,26 @@ void display(int* arr,int n)
   }
   
 }
-int linearSearch()
+int linearSearch(int* arr,int target,int size)
 {
-  // linear search code goes here
+  int ans=-1;
+  for (int  i = 0; i < size; i++)
+  {
+    if (arr[i]==target)
+    {
+      ans=i;
+      return i;
+    }
+    
+  }
+  return ans;
+  
 }
 int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   display(arr,9);
+  int idx=linearSearch(arr,10,9);
 
   return 0;
 }
