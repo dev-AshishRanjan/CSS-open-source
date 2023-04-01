ac74bc08ee487ee25d5b64490ac5f4f8f96e8f19
krishnam <maheshwarikrishnam123@gmail.com>
2023-04-01 19:18:36 +0530

    changes in linearsearch.c

--- linear-search.c
+++ linear-search.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
-void display()
+void display(int arr[])
 {
   // display code goes here
+     for(int i=0;i<9;i++)
+     {
+       printf("%d ",arr[i]);
+     }
 }
-int linearSearch()
+int linearSearch(int arr[])
 {
   // linear search code goes here
+       int n,i;
+        printf("Enter the number want to linear search");
+        scanf("%d",&n);
+         for( i=0;i<9;i++)
+         {
+           if(arr[i]==n)
+             {
+              printf("YES");
+              break;
+             }
+         }
+         if(i==9)
+          {
+            printf("NO");
+          }
 }
 int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
-
+       display(arr);
+       linearSearch(arr);
   return 0;
 }
