f8afa63f8a540c2853bc0e3f5a88703ef897587e
prince7703 <princekoshti7703@gmail.com>
2023-04-01 19:20:11 +0530

    change display function

--- linear-search.c
+++ linear-search.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-void display()
-{
-  // display code goes here
+void display(int arr[],int n){
+  for(int i=0;i<n;i++){
+ printf("%d ",arr[i]);
+  }
 }
+
 int linearSearch()
 {
   // linear search code goes here
@@ -11,6 +13,7 @@ int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
+  display(arr,9);
 
   return 0;
 }
