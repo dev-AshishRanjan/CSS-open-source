55bdb44f005a2ccaf3308becca97c4a59a1b4835
satya <satyaprakashpc2020@gmail.com>
2023-04-01 19:20:55 +0530

    a display function is added

--- linear-search.c
+++ linear-search.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-void display()
+void display(int *arr ,int x)
 {
-  // display code goes here
+  for(int i=0;i<x;i++){
+    printf("%d",arr[i]);
+  }
 }
 int linearSearch()
 {
@@ -10,7 +12,7 @@ int linearSearch()
 int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
-  // main program
+  display(arr,9)
 
   return 0;
 }
