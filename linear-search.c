30469b525d56c80e4a5bde118f09acc581859755
Suraj <iamsurajgupta1212@gmail.com>
2023-04-01 23:23:30 +0530

    code for linear search

--- linear-search.c
+++ linear-search.c
@@ -2,26 +2,30 @@
 void display()
 {
   // display code goes here
+
 }
 int linearSearch(int arr[], int key)
 {
   // linear search code goes here
-  for(int i=0; i<9; i++){
-    if(arr[i] == key){
-      printf("%d", i);
-    }
-    else
+  for(int i=0; i<9; i++)
+  {
+    if(arr[i] == key)
     {
-      printf("-1");
-
+      return i;
     }
   }
+  return -1;
 }
 int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
-  linearSearch(arr, 12);
+  int key;
+  scanf("%d", &key);
+  int search = linearSearch(arr, key);
+  printf("%d\n", search);
+  display(arr);
+
 
   return 0;
 }
