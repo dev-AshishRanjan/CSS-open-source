36b8bc49804582a96d76c5e66264e4f33c4ba5c2
Pragya <www.pprd@gmail.com>
2023-04-01 19:40:29 +0530

    Implemented Linear search

--- linear-search.c
+++ linear-search.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
-void display()
-{
-  // display code goes here
+void display(int i)
+{ 
+  int index=i;
+  printf("%d",index);
 }
-int linearSearch()
+int linearSearch(int arr[],int key,int n)
 {
+  for(int i=0;i<n;i++){
+    if(arr[i]==key){
+      display(i);
+    }else
+      display(-1);
+  }
   // linear search code goes here
 }
 int main()
-{
+{ int key;
+printf("Enter your key:");
+scanf("%d",&key);
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
+  linearSearch(arr[],key,9);
 
   return 0;
 }
