df38506c14607598ea81660e7f9f7718d83d14ac
Gaurav <gaiuravbanik2005@gmail.com>
2023-04-01 19:31:38 +0530

    linearsearch

--- linear-search.c
+++ linear-search.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-void display()
+void display(int *arr, int size)
 {
+  for (int i=0 ; i<size ; i++)
+  {
+    printf("%d\n",arr[i]);
+  }
   // display code goes here
 }
 int linearSearch()
@@ -11,6 +15,6 @@ int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
-
+display(arr , 9);
   return 0;
 }
