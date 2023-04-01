96e58f5025a8161696e3fa23a4389cbe44c4431e
Suraj <iamsurajgupta1212@gmail.com>
2023-04-01 19:52:17 +0530

    implemented linear search

--- linear-search.c
+++ linear-search.c
@@ -3,14 +3,25 @@ void display()
 {
   // display code goes here
 }
-int linearSearch()
+int linearSearch(int arr[], int key)
 {
   // linear search code goes here
+  for(int i=0; i<9; i++){
+    if(arr[i] == key){
+      printf("%d", i);
+    }
+    else
+    {
+      printf("-1");
+
+    }
+  }
 }
 int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
+  linearSearch(arr, 12);
 
   return 0;
 }
