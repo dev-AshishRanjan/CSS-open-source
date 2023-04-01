e9affe936ac0a7226b37ede1e8ff58df23576240
Sohail <sohailkazi001@gmail.com>
2023-04-01 19:23:41 +0530

    Created linearSearch and Display functions.

--- linear-search.c
+++ linear-search.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
-void display()
+void display(int flag)
 {
-  // display code goes here
+  if (flag)
+  {
+    printf("Item exists in array.\n");
+  }
+  else
+  {
+    printf("Item does not exist in array.\n");
+  }
 }
-int linearSearch()
+int linearSearch(int arr[], int n, int length)
 {
   // linear search code goes here
+  int flag = 1;
+  for(int i = 0; i < length; i++)
+  {
+    if(arr[i] == n)
+    {
+        display(1);flag = 0; break;
+    }
+  }
+
+  if(flag)
+    display(0);
 }
 int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
 
+  int n; 
+  printf("Enter value to search: ");
+  scanf("%d", &n);
+  linearSearch(arr,n, sizeof(arr)/sizeof(arr[0]));
   return 0;
 }
