79ab8afeee228594c15e51072f206e94f99c4245
Suraj <iamsurajgupta1212@gmail.com>
2023-04-02 11:01:26 +0530

    implemented display function

--- linear-search.c
+++ linear-search.c
@@ -12,23 +12,13 @@ void display(int arr[])
 int linearSearch(int arr[], int key)
 {
   // linear search code goes here
-  for(int i=0; i<9; i++)
-  {
-    if(arr[i] == key)
-    {
-      return i;
-    }
-  }
-  return -1;
+ 
 }
 int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
-  int key;
-  scanf("%d", &key);
-  int search = linearSearch(arr, key);
-  printf("%d\n", search);
+ 
   display(arr);
 
 
