290ffca733a6b7c338cdfdd476a6e349efa52601
prince7703 <princekoshti7703@gmail.com>
2023-04-05 16:50:00 +0530

    completed display function

--- linear-search.c
+++ linear-search.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-void display()
+void display(int arr[],int n)
 {
   // display code goes here
+
+  for(int i=0;i<n;i++){
+    printf("%d ",arr[i]);
+  }
+  printff("\n ");
+
 }
 int linearSearch(int search,int n,int arr[])
 {
@@ -19,6 +25,7 @@ int main()
 {
   int arr[] = {5, 3, 7, 9, 10, 12, 7, 2, 4};
   // main program
+  display(arr,9);
   printf("%d",linearSearch(7,sizeof(arr),arr));
   return 0;
 }
